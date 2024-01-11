2f01c936af4ec4bdc4e43cff26bf3f2cf7f13049
JoanVC100 <jojovica2001@gmail.com>
2024-01-11 15:58:23 +0100

    Transfer warning

--- examples/SimpleComm/RS485-SendAllTypes/PacketTypes.h
+++
@@ -1,1 +0,0 @@
-typedef enum {P_BOOL, P_CHAR, P_UCHAR, P_INT, P_UINT, P_LONG, P_ULONG, P_DOUBLE} packet_t;
--- src/utilities/crc16.h
+++
@@ -1,8 +0,0 @@
-#ifndef __utilities_Modbus_H__
-#define __utilities_Modbus_H__
-
-#include <inttypes.h>
-
-uint16_t crc16(const uint8_t *ptr, uint16_t len);
-
-#endif // __utilities_Modbus_H__
